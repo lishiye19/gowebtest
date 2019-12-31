dc39badd4417c86dd0e899c10065748df8917a49
lishiye19 <lishiye19@163.com>
2019-12-31 21:35:23 +0800

    Update main.go
    额外

--- gowebapp/main.go
+++ gowebapp/main.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	http.HandleFunc("/",handler)
-	http.ListenAndServe(":8080",nil)
+	_ = http.ListenAndServe(":8080", nil)
 }
 
 func handler(writer http.ResponseWriter,request *http.Request){
 	_, _ = fmt.Fprintf(writer, "hello world, %s!", request.URL.Path[1:])
 }
+
