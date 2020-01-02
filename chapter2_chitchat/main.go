a99577f3529ff33615fcfcbd538e061641711733
lishiye19 <lishiye19@163.com>
2020-01-02 21:56:59 +0800

    Update main.go

--- chapter2_chitchat/main.go
+++ chapter2_chitchat/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func main() {
 	mux:=http.NewServeMux()
+	files:=http.FileServer(http.Dir("/public"))
+	mux.HandleFunc("/",index)
+	mux.Handle("/static/",http.StripPrefix("/static/",files))
+}
+
+func index(w http.ResponseWriter,r *http.Request){
+	files:=[]string{
+		"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html",
+	}
+	templates:=template.Must(template.ParseFiles(files...))
+	threads,err:=data.Threads()
+	if err==nil{
+		templates.ExecuteTemplate(w,"layout",threads)
+	}
 }
