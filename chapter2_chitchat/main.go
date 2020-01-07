5b3f4fa4b1cacab1aa045db41cbe01013b5ccddc
lishiye19 <lishiye19@163.com>
2020-01-07 16:05:31 +0800

    Update main.go

--- chapter2_chitchat/main.go
+++ chapter2_chitchat/main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"chapter2_chitchat/data"
 )
 
 func main() {
 	mux:=http.NewServeMux()
 	files:=http.FileServer(http.Dir("/public"))
-	mux.HandleFunc("/",index)
 	mux.Handle("/static/",http.StripPrefix("/static/",files))
+	mux.HandleFunc("/",index)
+	server:=&http.Server{
+		Addr:              "0.0.0.0:8080",
+		Handler:           mux,
+		TLSConfig:         nil,
+		ReadTimeout:       0,
+		ReadHeaderTimeout: 0,
+		WriteTimeout:      0,
+		IdleTimeout:       0,
+		MaxHeaderBytes:    0,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+	server.ListenAndServe()
+
 }
 
 func index(w http.ResponseWriter,r *http.Request){
